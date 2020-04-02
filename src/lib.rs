ee6ce2b83ffcd75cd2dce0cef63b483251d4a3fc
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2020-04-02 23:47:34 +0700

    Add test: 3 Points colinear horizontal (0, 0), (0.1, 0.0), (0.2, 0.0)

--- src/lib.rs
+++ src/lib.rs
@@ -9,5 +9,18 @@ pub fn convex_hull(polygon: Vec<Complex64>) -> Result<Vec<Complex64>, String> {
         return Err(message);
     }
 
+    let origin_y: f64 = 0.0;
+    let mut same_y = true;
+    for i in 1..(polygon.len()) {
+        let point = polygon[i];
+        if point.im != origin_y {
+            same_y = false;
+            break;
+        }
+    }
+    if same_y {
+        return Result::Ok(Vec::new());
+    }
+
     return Result::Ok(polygon);
 }
--- tests/unit_tests.rs
+++ tests/unit_tests.rs
@@ -44,3 +44,15 @@ fn three_points_convex() {
     assert_eq!(result.is_ok(), true);
     assert_eq!(result.unwrap().len(), 3);
 }
+
+#[test]
+fn three_points_colinear_horizontal() {
+    let mut polygon: Vec<Complex64> = Vec::new();
+    polygon.push(Complex64::new(0.0, 0.0));
+    polygon.push(Complex64::new(0.1, 0.0));
+    polygon.push(Complex64::new(0.2, 0.0));
+
+    let result: Result<Vec<Complex64>, String> = convex_hull(polygon);
+    assert_eq!(result.is_ok(), true);
+    assert_eq!(result.unwrap().len(), 0);
+}
