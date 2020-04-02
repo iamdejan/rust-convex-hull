e618ad5ff4ad2e55dd4fd938e546b0a96ea8cc2d
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2020-04-02 23:52:40 +0700

    Add test: 3 Points colinear vertical (0, 0), (0.0, 0.1), (0.0, 0.2)

--- src/lib.rs
+++ src/lib.rs
@@ -9,17 +9,34 @@ pub fn convex_hull(polygon: Vec<Complex64>) -> Result<Vec<Complex64>, String> {
         return Err(message);
     }
 
-    let origin_y: f64 = 0.0;
-    let mut same_y = true;
-    for i in 1..(polygon.len()) {
-        let point = polygon[i];
-        if point.im != origin_y {
-            same_y = false;
-            break;
+    {
+        let origin_y: f64 = polygon[0].im;
+        let mut same_y = true;
+        for i in 1..(polygon.len()) {
+            let point = polygon[i];
+            if point.im != origin_y {
+                same_y = false;
+                break;
+            }
+        }
+        if same_y {
+            return Result::Ok(Vec::new());
         }
     }
-    if same_y {
-        return Result::Ok(Vec::new());
+
+    {
+        let origin_x: f64 = polygon[0].re;
+        let mut same_x = true;
+        for i in 1..(polygon.len()) {
+            let point = polygon[i];
+            if point.re != origin_x {
+                same_x = false;
+                break;
+            }
+        }
+        if same_x {
+            return Result::Ok(Vec::new());
+        }
     }
 
     return Result::Ok(polygon);
