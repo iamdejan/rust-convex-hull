a138393082897d0b38ee8820c7caa320fca1d941
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2020-04-03 20:10:30 +0700

    Add test: 4 Points colinear vertical (0, 0), (0.0, 0.1), (0.0, 0.2), (0.0, 0.3)

--- tests/unit_tests.rs
+++ tests/unit_tests.rs
@@ -81,3 +81,16 @@ fn four_points_colinear_horizontal() {
     assert_eq!(result.is_ok(), true);
     assert_eq!(result.unwrap().len(), 0);
 }
+
+#[test]
+fn four_points_colinear_vertical() {
+    let mut polygon: Vec<Complex64> = Vec::new();
+    polygon.push(Complex64::new(0.0, 0.0));
+    polygon.push(Complex64::new(0.0, 0.1));
+    polygon.push(Complex64::new(0.0, 0.2));
+    polygon.push(Complex64::new(0.0, 0.3));
+
+    let result: Result<Vec<Complex64>, String> = convex_hull(polygon);
+    assert_eq!(result.is_ok(), true);
+    assert_eq!(result.unwrap().len(), 0);
+}
